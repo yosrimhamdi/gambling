67862a8ddc2a7dc904ef96ce7d36d85dbd54c9fb
yosrimhamdi <yosri@mhamdi.co>
2022-12-18 13:43:28 +0100

    Initial commit.

---
+++ bs.js
@@ -0,0 +1,7 @@
+module.exports = {
+  server: '.',
+  files: 'src/**',
+  notify: false,
+  startPath: '/src/html/landing-page-1.html',
+  reloadDelay: 200,
+};
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    'landing-page-1': '/src/sass/landing-page-1.sass',
+  },
+  output: {
+    path: __dirname + '/dist/',
+  },
+  mode: process.env.NODE_ENV,
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  devServer: {
+    devMiddleware: {
+      writeToDisk: true,
+    },
+  },
+};
