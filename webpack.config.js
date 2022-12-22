148208dfb7b85d364153cd89c421b7769154ac39
yosrimhamdi <yosri@mhamdi.co>
2022-12-22 14:47:31 +0100

    client change request.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -18,6 +19,14 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: '[name].css',
     }),
+    // ...[1, 2, 3, 4].map(
+    //   i =>
+    //     new HtmlWebpackPlugin({
+    //       template: `src/html/landing-page-${i}.html`,
+    //       filename: `landing-pages/landing-page-${i}.html`,
+    //       inject: false,
+    //     })
+    // ),
   ],
   module: {
     rules: [
@@ -35,6 +44,32 @@ module.exports = {
         test: /\.s[ac]ss$/i,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
+      // {
+      //   test: /\.html$/i,
+      //   use: 'html-loader',
+      // },
+      // {
+      //   test: /\.(png|jpg|svg)$/i,
+      //   type: 'asset/resource',
+      //   use: [
+      //     {
+      //       loader: 'image-webpack-loader',
+      //       options: {
+      //         pngquant: {
+      //           quality: [0.9, 0.95],
+      //         },
+      //       },
+      //     },
+      //   ],
+      //   parser: {
+      //     dataUrlCondition: {
+      //       maxSize: 10 * 1024,
+      //     },
+      //   },
+      //   generator: {
+      //     filename: 'images/[name]-[hash][ext]',
+      //   },
+      // },
     ],
   },
   devServer: {
